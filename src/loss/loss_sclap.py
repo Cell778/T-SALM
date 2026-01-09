eb0348966bdbd1539970f5a541a755c3ad57902e
cell778 <406280340@qq.com>
2026-01-09 15:25:38 +0800

    2-way temporal loss

--- src/loss/loss_sclap.py
+++ src/loss/loss_sclap.py
@@ -22,7 +22,7 @@ class sCLAPLoss:
           (we keep only positive text as anchors). In this mode we:
             * compute spatial/semantic losses on all 3B samples(3B vs 3B)
             * compute temporal loss as 2-way, only compute similartiy between pos_audio,pos_text(anchor),neg_t_audio
-              per anchor text_sed (B vs 2B)
+              per anchor text_sed (3B -> 2-way clasification)
         """
 
         weights_all = list(self.weights)
